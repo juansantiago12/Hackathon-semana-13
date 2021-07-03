89a368ee290dd99199486017c915bc81950c6baa
Juan Santiago <jsantiagovalerio@gmail.com>
2021-07-03 12:30:06 -0500

    solucion del Tercer Reto

---
+++ app.js
@@ -0,0 +1,45 @@
+const cargar = document.getElementById('btnGet');
+const listado = document.querySelector('.container-cards');
+
+
+cargar.addEventListener('click', getInfo);
+listado.addEventListener('click', deleteInfo);
+let articulosPerritos = [];
+
+function getInfo(){
+    let template = '';
+    infoPerritos.forEach((infoPerrito) => {
+        template += `
+            <div class="card">
+                <a href="#" class="borrar">X</a>
+                <img src="${infoPerrito.imagen}" alt="perro 1">
+                <h3>Nombre: <span>${infoPerrito.nombre}</span></h3>
+                <h3>Telefono: <span>${infoPerrito.telefono}</span></h3>
+                <h3>Pais: <span>${infoPerrito.pais}</span></h3>
+                <p>${infoPerrito.about}</p>
+            </div>
+        `;
+    });
+    listado.innerHTML = template;
+}
+
+function deleteInfo(e){
+    if(e.target.classList.contains('borrar')){
+        swal({
+            title: "Estas seguro?",
+            text: "'Esta seguro que quiere eliminar esta mascota'",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+        .then((willDelete) => {
+            if (willDelete) {
+                const articuloActual = e.target.parentElement;
+                articuloActual.remove();
+                swal("Tu mascota a sido eliminada",{
+                icon: "success",
+                });
+            }
+        });
+    }
+}
---
+++ data.js
@@ -0,0 +1,57 @@
+const infoPerritos = [
+    {   imagen: '/img/perro1.jpg',
+        nombre: 'Yorkshire terrier',
+        telefono: 1234,
+        pais: 'Inglaterra',
+        about: 'Es un perro de tamaño pequeño. Si estás pensando en adoptar a uno, es muy importante que antes investigues acerca del carácter del Yorkshire terrier y otros factores relacionados de un Yorkshire.'
+    },
+    {
+        imagen: '/img/perro2.jpg',
+        nombre: 'Basenji',
+        telefono: 5678,
+        pais: 'Africa',
+        about: 'es uno de los perros más antiguos que existen actualmente. El basenji es inteligente, equilibrado y tiene dos características peculiares: nunca ladra y las hembras entran en celo una sola vez al año.'
+    },
+    {
+        imagen: '/img/perro3.jpg',
+        nombre: 'Bull arab',
+        telefono: 91011,
+        pais: 'Australia',
+        about: 'Son perros grandes, fuertes y con potentes instintos de caza, especialmente la caza de jabalíes, por lo que necesitan ejercitarse diariamente.'
+    },
+    {
+        imagen: '/img/perro4.jpg',
+        nombre: 'Spaniel Tibetano',
+        telefono: 121314,
+        pais: 'China',
+        about: 'son unos perros asiáticos de pequeño tamaño y maravilloso carácter. Son buenos perros de compañía, no requieren de mucha actividad física y los cuidados no son muy diferentes al del resto de los perros.'
+    },
+    {
+        imagen: '/img/perro5.jpg',
+        nombre: 'Vallhund sueco',
+        telefono: 151617,
+        pais: 'Suecia',
+        about: 'También llamado pastor sueco, es un pequeño perro que se originó hace muchos siglos en Suecia. Destinado al pastoreo, protección y a la caza de pequeños animales.'
+    },
+    {
+        imagen: '/img/perro6.jpg',
+        nombre: 'Poochon',
+        telefono: 181920,
+        pais: 'Australia',
+        about: 'Es una raza híbrida entre un caniche y un bichon frisé originaria de Australia. El poochon es un perro enérgico, sociable, cariñoso, juguetón, muy leal y dependiente de sus cuidadores, tanto que puede sufrir de ansiedad por separación.'
+    },
+    {
+        imagen: '/img/perro7.jpg',
+        nombre: 'Shepsky',
+        telefono: 212223,
+        pais: 'Estados Unidos',
+        about: 'e trata de perros que heredan las características físicas y comportamentales de sus progenitores, siendo grandes, estilosos, fuertes, activos, inteligentes, obedientes, comprometidos, leales y amigables.'
+    },
+    {
+        imagen: '/img/perro8.jpg',
+        nombre: 'Pastor leonés',
+        telefono: 242526,
+        pais: 'España',
+        about: 'es un perro idóneo para el pastoreo del ganado, especialmente de ovejas. Es propio de Castilla y León y nunca se ha llegado a adaptar a la vida de hogar, ya que es un perro que necesita ejercitarse y estar en el exterior.'
+    }
+]
